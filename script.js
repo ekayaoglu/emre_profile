2b60fdfa9e1922cb56a16d3e4265d5b42fe0b724
EmreK <95968154+ekayaoglu@users.noreply.github.com>
2022-05-08 18:06:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    document.body.style.backgroundColor = 'white'; })
+
+    const btn2 = document.getElementById('btn2');
+
+btn2.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    document.body.style.backgroundColor = '#0C2233'; })
